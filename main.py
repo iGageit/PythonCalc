6b21b9928f5c23a0724ca4079701467e001b7924
Gage Milton <richardson.milton@my.tccd.edu>
2019-12-13 21:29:17 +0000

    added comments

--- main.py
+++ main.py
@@ -2,10 +2,12 @@ num1 = float(input("Enter first number: "))
 op = input("Enter Operator: ") 
 num2 = float(input("Enter Second number: ")) 
 
+#this is a comment 
 
+# If statements if Python
 if op == "+" :
   print(num1 + num2)
-elif op == "-" :
+elif op == "-" : #keep elif in the left corner. 
     print(num1 - num2)
 elif op == "/":
     print( num1 / num2)
@@ -13,3 +15,5 @@ elif op == "*":
     print(num1 * num2)
 else: 
       print ("sorry try again")
+
+      # make sure to have your code properly indented, this messed up my code. 
