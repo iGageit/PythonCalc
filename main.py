28ea1e4e3244556cab9232f6ae0e0a654ffea86a
Gage Milton <richardson.milton@my.tccd.edu>
2019-12-13 21:27:29 +0000

    Initial commit

---
+++ main.py
@@ -0,0 +1,15 @@
+num1 = float(input("Enter first number: ")) 
+op = input("Enter Operator: ") 
+num2 = float(input("Enter Second number: ")) 
+
+
+if op == "+" :
+  print(num1 + num2)
+elif op == "-" :
+    print(num1 - num2)
+elif op == "/":
+    print( num1 / num2)
+elif op == "*":
+    print(num1 * num2)
+else: 
+      print ("sorry try again")
